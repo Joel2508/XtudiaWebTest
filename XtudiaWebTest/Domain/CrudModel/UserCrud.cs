4a9193b5782fe710f3f62a3f0ca35099f36b35ce
ing.joel2508 joel <xtudia.ing.joelarias@hotmail.com>
2017-12-07 16:21:47 -0600

    Delete the oline code

--- XtudiaWebTest/Domain/CrudModel/UserCrud.cs
+++ XtudiaWebTest/Domain/CrudModel/UserCrud.cs
@@ -34,7 +34,6 @@ namespace Domain.CrudModel
                                 ve.PropertyName, ve.ErrorMessage);
                         }
                     }
-                    return e.Message(message);
                 }
             }
         }
